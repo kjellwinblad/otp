caddd4e0c36a78a3385f4dcfcb9e7b84e935365d
David Klaftenegger <david.klaftenegger@it.uu.se>
2013-01-15 15:20:09 +0100

    script changed to use c++-enabled c_maps

--- fetch_latest_c_maps_and_put_in_beam_dir.sh
+++ fetch_latest_c_maps_and_put_in_beam_dir.sh
@@ -4,7 +4,7 @@ BEAM_DIR=erts/emulator/beam
 
 rm -rf c_maps
 
-git clone git://github.com/kjellwinblad/c_maps.git
+git clone git://github.com/davidklaftenegger/c_maps.git
 
 cp c_maps/*.c $BEAM_DIR
 cp c_maps/*.h $BEAM_DIR
