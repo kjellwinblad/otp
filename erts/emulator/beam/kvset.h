858720d3f952a0e40b87a6f3923875e58863a115
David Klaftenegger <david.klaftenegger@it.uu.se>
2013-01-24 18:01:12 +0100

    copy over kvset.h from c_maps project branch

--- erts/emulator/beam/kvset.h
+++ erts/emulator/beam/kvset.h
@@ -1,22 +1,27 @@
 #ifndef __KVSET_H__
 #define __KVSET_H__
 
-typedef struct kv_set KVSet;
+#ifdef __cplusplus
+/* allow this header to be used verbatim in C++ code */
+extern "C" {
+#endif
+
+struct kv_set;
 
 typedef struct kv_set_functions
 {   
-    void (*delete_table)(KVSet* kv_set,
+    void (*delete_table)(struct kv_set * kv_set,
                          void (*element_free_function)(void *context, void* element),
                          void * context);
-    void * (*put)(KVSet * set, void * key_value);
-    int (*put_new)(KVSet * set, void * key_value);
-    void * (*remove)(KVSet * set, void * key);
-    void * (*lookup)(KVSet * set, void * key);
-    int (*member)(KVSet * set, void * key);
-    void * (*first)(KVSet * set);
-    void * (*last)(KVSet * set);    
-    void * (*next)(KVSet * set, void * key);
-    void * (*previous)(KVSet * set, void * key);
+    void * (*put)(struct kv_set * kv_set, void * key_value);
+    int (*put_new)(struct kv_set * kv_set, void * key_value);
+    void * (*remove)(struct kv_set * kv_set, void * key);
+    void * (*lookup)(struct kv_set * kv_set, void * key);
+    int (*member)(struct kv_set * kv_set, void * key);
+    void * (*first)(struct kv_set * kv_set);
+    void * (*last)(struct kv_set * kv_set);    
+    void * (*next)(struct kv_set * kv_set, void * key);
+    void * (*previous)(struct kv_set * kv_set, void * key);
 } KVSetFunctions;
 
 
@@ -26,5 +31,10 @@ typedef struct kv_set
     unsigned int key_offset;
     void * type_specific_data;
 } KVSet;
+
+#ifdef __cplusplus
+/* see top, C++ compatibility */
+}
+#endif
 
 #endif
