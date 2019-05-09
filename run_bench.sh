93d172d3f51378b73ffa6b1131a4e6a83d29d203
Kjell Winblad <kjellwinblad@gmail.com>
2019-05-09 13:50:26 +0200

    Update benchmark config

--- run_bench.sh
+++ run_bench.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
+
+
+
 sudo apt-get update
 
-sudo apt-get install -y xsltproc fop openssl libssl-dev unixodbc-dev autoconf build-essential m4 libssh-dev unixodbc-dev libncurses5-dev libncursesw5-dev
+sudo apt-get install -y xsltproc fop openssl libssl-dev unixodbc-dev autoconf build-essential m4 libssh-dev unixodbc-dev libncurses5-dev libncursesw5-dev numactl tmux htop
+
+numactl --hardware >> ets_SUITE_data/numactl_hardware
+
+cat /proc/cpuinfo >> ets_SUITE_data/proc_cpuinfo
+
+cat /proc/meminfo >> ets_SUITE_data/proc_meminfo
+
+lsb_release -a >> ets_SUITE_data/os_info
+
+echo LINUX >> ets_SUITE_data/os_info
+
+uname -r >> ets_SUITE_data/os_info
 
 git clean -d -x -f
 
@@ -10,7 +25,7 @@ source otp_env.source
 
 ./otp_build autoconf
 
-./configure
+./configure --with-ets-write-concurrency-locks=256
 
 make
 
@@ -18,4 +33,8 @@ source otp_env.source
 
 erlc ets_SUITE.erl
 
-erl +sbt nnts -eval "ets_SUITE:throughput_benchmark(),erlang:halt()"
+# Spread as much as possible
+erl +sbt s -eval "ets_SUITE:throughput_benchmark(),erlang:halt()"
+
+# Same as db (fills one chip at a time)
+erl +sbt tnnps -eval "ets_SUITE:throughput_benchmark(),erlang:halt()"
