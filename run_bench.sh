6bd65f8e19ad7e331ea8b525ec6d2ff7df5e4a2f
Kjell Winblad <kjellwinblad@gmail.com>
2019-05-09 09:58:15 +0200

    Prepare for benchmark

---
+++ run_bench.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get install -y xsltproc fop openssl libssl-dev unixodbc-dev autoconf build-essential m4 libssh-dev unixodbc-dev libncurses5-dev libncursesw5-dev
+
+git clean -d -x -f
+
+source otp_env.source
+
+./otp_build autoconf
+
+./configure
+
+make
+
+source otp_env.source
+
+erlc ets_SUITE.erl
+
+erl -eval "ets_SUITE:throughput_benchmark(),erlang:halt()"
